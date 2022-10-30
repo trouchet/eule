7807302f5d5ddad2026e2f8062c5a75beb6cbdfe
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-30 20:43:17 -0300

    test/refac/ adequate diagram key string for alphabetically sorting

--- tests/test_euler.py
+++ tests/test_euler.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
+import pytest
+
 """Tests for `eule` package."""
 from eule import euler, spread_euler
 
 def test_euler_iter_1_input():
-    assert next(euler({'a': [1,2]})) == ('a', [1,2])
+    assert next(euler({'a': [1, 2]})) == ('a', [1, 2])
 
 def test_euler_iter_2_input():
-    eu_fun = euler({'a': [1,2], 'b': [2,3]})
+    eu_fun = euler({'a': [1, 2], 'b': [2, 3]})
     
     assert next(eu_fun) == ('b', [3])
     assert next(eu_fun) == ('a,b', [2])
@@ -50,8 +52,7 @@ def test_spread_euler_3_sets():
             'a': [1, 2, 3], 
             'b': [2, 3, 4], 
             'c': [3, 4, 5]
-        }
-    ) == {'a,b': [2], 'c,b': [4], 'a,c,b': [3], 'c': [5], 'a': [1]}
+        }) == {'a,b': [2], 'b,c': [4], 'a,b,c': [3], 'c': [5], 'a': [1]}
 
 def test_spread_euler_4_sets():
     assert spread_euler(
@@ -60,7 +61,5 @@ def test_spread_euler_4_sets():
             'b': [2, 3, 4], 
             'c': [3, 4, 5],
             'd': [3, 5, 6]
-        }
-    ) == {'a,b': [2], 'c,b': [4], 'a,d,c,b': [3], 'd,c': [5], 'd': [6], 'a': [1]}
-
+        }) == {'a,b': [2], 'b,c': [4], 'a,b,c,d': [3], 'c,d': [5], 'd': [6], 'a': [1]}
     
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,14 +3,17 @@
 """Tests for `eule` package."""
 
 import pytest
-from eule.utils import keyfy, reduce_, unique
+from eule.utils import keyfy, reduce_, unique, delimited_sort
 
 # define the tests
 def test_keyfy():
     assert keyfy([1, 2, 3]) == '1,2,3'
 
 def test_one_set_euler():
-    assert reduce(lambda a, b: a+b, [1, 2], 0) == 3
+    assert reduce_(lambda a, b: a+b, [1, 2], 0) == 3
 
 def test_unique_elems():
-    assert unique([1, 2, 3]) == [1,2,3]
+    assert unique([1, 2, 3]) == [1, 2, 3]
+
+def test_unique_elems():
+    assert delimited_sort('4,1,2,3', ',') == '1,2,3,4'
