c6e008fe1564dac1cfa748f2053016a6f7b2a7e8
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-31 11:27:54 -0300

    refac/ pep8

--- eule/eule.py
+++ eule/eule.py
@@ -1,13 +1,16 @@
 """Main module."""
+from __future__ import unicode_literals, absolute_import
+
 from warnings import warn
 
-from .utils import keyfy, reduce_, unique, delimited_sort
+from .utils import reduce_, unique, delimited_sort, non_empty_sets_keys
+
 
 def euler(sets):
     '''
         @abstract returns each tuple [key, elems] of the Euler diagram
         systematic in a generator-wise fashion
-        Rationale: 
+        Rationale:
            1. Begin with the available sets and their exclusive elements;
            2. Compute complementary elements to current key-set;
            3. In case complementary set-keys AND current set content are not empty, continue;
@@ -21,37 +24,34 @@ def euler(sets):
         @param {Array} sets
         @return {Array} keys_elems
     '''
-    set_keys_ = keyfy(list(sets.keys()))
-    
     is_unique_set_arr = [len(unique(values)) == len(values) for values in sets.values()]
     if not reduce_(lambda a, b: a and b, is_unique_set_arr, True):
         warn("Each array MUST NOT have duplicates")
         sets = {key: unique(values) for key, values in sets.items()}
-    
+
     # Only a set
     if len(sets.values()) == 1:
         key = list(sets.keys())[0]
         value = list(sets.values())[0]
         yield (key, value)
-    
+
     else:
         # There are no sets
         if not isinstance(sets, (list, dict)):
             raise Exception("Ill-conditioned input.")
-        
-        # Remove empty sets
-        sets_clear = lambda sets_: list(filter(lambda key: len(sets_[key]) != 0, sets_.keys()))
 
-        # Keys complementary to current available key-set 
+        # Remove empty sets
+
+        # Keys complementary to current available key-set
         compl_sets_keys = []
 
         # Sets with non-empty elements
-        set_keys = sets_clear(sets)
+        set_keys = non_empty_sets_keys(sets)
 
-        # Traverse the combination lattice 
+        # Traverse the combination lattice
         for set_key in set_keys:
             compl_sets_keys = list(set(set_keys) - set([set_key]))
-            
+
             # There are still sets to analyze
             # Morgan Rule: ¬(A & B) = ¬A | ¬B
             if (len(compl_sets_keys) != 0 and len(sets[set_key]) != 0):
@@ -60,7 +60,7 @@ def euler(sets):
                 for comb_str, celements in euler(csets):
                     # Exclusive combination elements
                     comb_excl = list(set(celements)-set(sets[set_key]))
-                    
+
                     # Non-empty combination exclusivity case
                     if len(comb_excl) != 0:
                         # 1. Exclusive group elements except current analysis set
@@ -68,7 +68,7 @@ def euler(sets):
 
                         for ckey in comb_str.split(','):
                             sets[ckey] = list(set(sets[ckey])-set(comb_excl))
-                    
+
                     comb_intersec = list(set(celements).intersection(set(sets[set_key])))
                     sets[set_key] = list(set(sets[set_key]) - set(comb_intersec))
 
@@ -77,24 +77,24 @@ def euler(sets):
                         comb_intersec_key = set_key+','+comb_str
                         yield (delimited_sort(comb_intersec_key, ','), comb_intersec)
 
-                        # Remove intersection elements from current key-set and complementary sets 
+                        # Remove intersection elements from current key-set and complementary sets
                         for ckey in comb_str.split(','):
                             sets[ckey] = list(set(sets[ckey])-set(comb_intersec))
 
                         sets[set_key] = list(set(sets[set_key])-set(comb_intersec))
-                
-                set_keys = sets_clear(sets)
+
+                set_keys = non_empty_sets_keys(sets)
 
                 # 3. Set-key exclusive elements
                 if len(sets[set_key]) != 0:
                     yield (str(set_key), sets[set_key])
 
-def spread_euler(sets): 
+
+def spread_euler(sets):
     '''
         @abstract returns Euler diagram dictionary of set-dictionary of non-repetitive elements
-        
-        
+
         @param {Array} sets
         @return {dict} euler_diagram
     '''
-    return dict(euler(sets))  
+    return dict(euler(sets))
