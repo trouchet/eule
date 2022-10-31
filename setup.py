38634394d591bdbb3adde333ab59620a5e5b626b
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-31 16:55:03 -0300

    config/ setup file for package deployment

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='eule',
     name='eule',
@@ -44,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/brunolnetto/eule',
-    version='0.1.0',
+    version='0.1.2',
     zip_safe=False,
 )
