55a231015824f5d5ab71fccd61954e038a7b7794
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-31 11:27:22 -0300

    feat/ set keys with empty list as values

--- eule/utils.py
+++ eule/utils.py
@@ -1,19 +1,21 @@
 import functools
 import numpy as np
 
+
 def keyfy(lst):
     """
         @abstract returns array entries in string fashion delimited by commas
-        
+
         @param {Array} arr
         @return {string} str
     """
     return str(lst).strip('[]').replace(' ', '')
 
-def reduce_(func, elems, elem0): 
+
+def reduce_(func, elems, elem0):
     """
         @abstract returns reduce function handler
-        
+
         @param {function} func
         @param {object} elems
         @param {object} elem0
@@ -21,10 +23,11 @@ def reduce_(func, elems, elem0):
     """
     return functools.reduce(func, elems+[elem0])
 
-def unique(lst): 
+
+def unique(lst):
     """
         @abstract returns list with unique elements
-        
+
         @param {array} arr
         @return {array} sorted_arr
     """
@@ -34,8 +37,24 @@ def unique(lst):
 def delimited_sort(str_, delimiter):
     """
         @abstract returns a sorted string delimited by token
-        
+
         @param {array} arr
         @return {array} sorted_arr
     """
     return delimiter.join(sorted(str_.split(delimiter)))
+
+
+def non_empty_sets_keys(sets):
+    """
+        @abstract returns a set with non-empty values
+
+        @param {dict} set
+        @return {dict} cleaned_set
+    """
+
+    return list(
+        filter(
+            lambda key: len(sets[key]) != 0,
+            sets.keys()
+        )
+    )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,18 +2,28 @@
 
 """Tests for `eule` package."""
 
-import pytest
-from eule.utils import keyfy, reduce_, unique, delimited_sort
+from eule.utils import keyfy, reduce_, unique, delimited_sort, non_empty_sets_keys
+
 
 # define the tests
 def test_keyfy():
     assert keyfy([1, 2, 3]) == '1,2,3'
 
+
 def test_one_set_euler():
     assert reduce_(lambda a, b: a+b, [1, 2], 0) == 3
+
 
 def test_unique_elems():
     assert unique([1, 2, 3]) == [1, 2, 3]
 
-def test_unique_elems():
+
+def test_delimited_sort():
     assert delimited_sort('4,1,2,3', ',') == '1,2,3,4'
+
+
+def test_non_empty_sets_keys():
+    assert non_empty_sets_keys({
+        'a': [1, 2, 3],
+        'b': []
+        }) == ['a']
