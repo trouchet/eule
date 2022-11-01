500da6f498b1c6047c9d56281cadac70fd2317b1
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-31 21:53:27 -0300

    feat/ remove unnecessary configurations

--- setup.py
+++ setup.py
@@ -7,34 +7,30 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = []
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ['pytest>=2', ]
 
 setup(
     author="Bruno Peixoto",
     author_email='brunolnetto@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=2.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     description="Euler's diagrams are non-empty Venn's diagrams",
-    entry_points={
-        'console_scripts': [
-            'eule=eule.cli:main',
-        ],
-    },
-    install_requires=requirements,
+    install_requires=requirements_dev,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
@@ -43,8 +39,7 @@ setup(
     name='eule',
     packages=find_packages(include=['eule', 'eule.*']),
     test_suite='tests',
-    tests_require=test_requirements,
     url='https://github.com/brunolnetto/eule',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
