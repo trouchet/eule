729c268fd4ab86fdb60873720141e84dbdb17587
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-01 16:14:45 -0300

    docs/ add docstring to tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,22 +7,37 @@ from eule.utils import keyfy, reduce_, unique, delimited_sort, non_empty_sets_ke
 
 # define the tests
 def test_keyfy():
+    '''
+    tests the key string generation
+    '''
     assert keyfy([1, 2, 3]) == '1,2,3'
 
 
 def test_one_set_euler():
+    '''
+    tests the reduce function
+    '''
     assert reduce_(lambda a, b: a+b, [1, 2], 0) == 3
 
 
 def test_unique_elems():
-    assert unique([1, 2, 3]) == [1, 2, 3]
+    '''
+    tests the reduce function
+    '''
+    assert unique([1, 2, 3, 3]) == [1, 2, 3]
 
 
 def test_delimited_sort():
+    '''
+    tests sorting delimited by token 
+    '''
     assert delimited_sort('4,1,2,3', ',') == '1,2,3,4'
 
 
 def test_non_empty_sets_keys():
+    '''
+    tests dict clean with empty values 
+    '''
     assert non_empty_sets_keys({
         'a': [1, 2, 3],
         'b': []
