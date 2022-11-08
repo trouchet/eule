225d1b44156e64645109dcca152b8284d5f38e32
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-08 15:13:36 -0300

    config/ setup file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = []
@@ -15,19 +15,27 @@ requirements = []
 test_requirements = ['pytest>=2', ]
 
 setup(
-    author="Bruno Peixoto",
+    name='eule',
+    author='Bruno Peixoto',
     author_email='brunolnetto@gmail.com',
     python_requires='>=2.7',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta'
+        'Framework :: tox'
+        'Intended Audience :: Developers'
         'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent'
+        'Topic :: Software Development :: Testing'
+        'Topic :: Software Development :: Libraries'
+        'Topic :: Utilities'
+        'Programming Language :: Python :: 2'
+        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
     ],
     description="Euler's diagrams are non-empty Venn's diagrams",
     install_requires=requirements,
@@ -35,12 +43,12 @@ setup(
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
     include_package_data=True,
-    keywords='eule',
-    name='eule',
-    packages=find_packages(include=['eule', 'eule.*']),
+    keywords=['euler', 'venn', 'diagrams'],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/quivero/eule',
-    version='0.1.6',
+    url='http://eule.readthedocs.org',
+    version='0.1.8',
     zip_safe=False,
 )
