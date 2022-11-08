6f33327703a44aad7e3cf7b55a5bedd8a471613b
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-08 17:35:21 -0300

    config/ update init file

--- src/eule/__init__.py
+++ src/eule/__init__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-
 """ Package Eule  """
 
-from __future__ import unicode_literals, absolute_import
 from .eule import euler, spread_euler
 
 __author__ = """Bruno Peixoto"""
 __email__ = 'brunolnetto@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.1.8'
 
 
-__all__ = ['spread_euler', 'euler']
+__all__ = [
+	'spread_euler', 'euler'
+]
