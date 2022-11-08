b77bcf96028900fd99a0582d8b26dd6519c62cf5
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-08 11:57:53 -0300

    docs/ logo

--- docs/conf.py
+++ docs/conf.py
@@ -83,18 +83,31 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+ html_theme_options = {
+    "canonical_url": "",
+    "logo_only": True,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 6,
+    "includehidden": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_last_updated_fmt = TODAY.isoformat()
+
+html_logo = "eule_small.png"
 
 
 # -- Options for HTMLHelp output ---------------------------------------
