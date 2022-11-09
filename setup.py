c33f905f91494a338be6e5f7e7e01b19b65aaa21
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-09 10:19:06 -0300

    fix/ classifiers

--- setup.py
+++ setup.py
@@ -20,22 +20,15 @@ setup(
     author_email='brunolnetto@gmail.com',
     python_requires='>=2.7',
     classifiers=[
-        'Development Status :: 4 - Beta'
-        'Framework :: tox'
-        'Intended Audience :: Developers'
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Scientists',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
-        'Topic :: Software Development :: Testing'
-        'Topic :: Software Development :: Libraries'
-        'Topic :: Utilities'
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.4'
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Euler's diagrams are non-empty Venn's diagrams",
     install_requires=requirements,
