696429f202ec2187cbe60c45beadb24e925ed088
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-22 10:17:14 -0300

    test/ eule

--- src/eule/__init__.py
+++ src/eule/__init__.py
@@ -1,12 +1,7 @@
 """ Package Eule  """
 
-from eule import euler, spread_euler
+from .eule import euler, spread_euler
 
 __author__ = """Bruno Peixoto"""
 __email__ = 'brunolnetto@gmail.com'
-__version__ = '0.1.8'
-
-
-__all__ = [
-    'spread_euler', 'euler'
-]
+__version__ = '0.2.0'
