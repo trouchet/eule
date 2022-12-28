93a6b4e826676cb1912ea32c50be95f297caabe3
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-28 16:27:53 -0300

    feat/ example

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,8 @@
 
 from __future__ import annotations
 
+from .eule import spread_euler
+
 __author__ = """Bruno Peixoto"""
 __email__ = "brunolnetto@gmail.com"
 __version__ = "0.2.0"
---
+++ src/examples/example.py
@@ -0,0 +1,13 @@
+from src.eule import spread_euler
+
+set = {
+    'a': [1, 2, 3],
+    'b': [2, 3, 4],
+    'c': [3, 4, 5],
+    'd': [3, 5, 6]
+}
+
+diagram = spread_euler(set)
+
+# Euler dictionary: {'a,b': [2], 'b,c': [4], 'a,b,c,d': [3], 'c,d': [5], 'd': [6], 'a': [1]}
+print(diagram)
