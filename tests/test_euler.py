3425c2c97ef7d2dd6b60121d99de42f41d912b8f
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-28 14:46:08 -0300

    test/

--- tests/test_euler.py
+++ tests/test_euler.py
@@ -1,114 +1,111 @@
+from __future__ import annotations
+
 import pytest
 
-from src.eule import *
+from src.eule import euler, spread_euler
 
 
 def test_euler_iter_1_input():
-    '''
+    """
     Generates a tuple with key-value
-    '''
-    assert next(euler({'a': [1, 2]})) == ('a', [1, 2])
+    """
+
+    assert next(euler({"a": [1, 2]})) == ("a", [1, 2])
 
 
 def test_euler_iter_2_input():
-    '''
+    """
     Generates all tuples with key-value
-    '''
-    eu_fun = euler({'a': [1, 2], 'b': [2, 3]})
+    """
+    eu_fun = euler({"a": [1, 2], "b": [2, 3]})
 
-    assert next(eu_fun) == ('b', [3])
-    assert next(eu_fun) == ('a,b', [2])
-    assert next(eu_fun) == ('a', [1])
+    assert next(eu_fun) == ("b", [3])
+    assert next(eu_fun) == ("a,b", [2])
+    assert next(eu_fun) == ("a", [1])
 
 
 def test_euler_iter_warning_1item():
-    '''
+    """
     Raises a warning for duplicated dict values
-    '''
+    """
     with pytest.warns(UserWarning):
-        next(euler({'a': [42, 42]}))
+        next(euler({"a": [42, 42]}))
 
 
 def test_euler_iter_warning_2items():
-    '''
+    """
     Raises a warning for duplicated dict values
-    '''
+    """
     with pytest.warns(UserWarning):
-        next(euler({'a': [42, 42], 'b': [42, 42]}))
+        next(euler({"a": [42, 42], "b": [42, 42]}))
 
 
 def test_spread_euler_ill_input_str():
-    '''
+    """
     Raises an Exception for ill-conditioned input as string
-    '''
-    with pytest.raises(TypeError, match='Ill-conditioned input.'):
-        spread_euler('')
+    """
+    with pytest.raises(TypeError, match="Ill-conditioned input."):
+        spread_euler("")
 
 
 def test_spread_euler_ill_input_num():
-    '''
+    """
     Raises an Exception for ill-conditioned input as number
-    '''
-    with pytest.raises(TypeError, match='Ill-conditioned input.'):
+    """
+    with pytest.raises(TypeError, match="Ill-conditioned input."):
         spread_euler(1)
 
 
 def test_spread_euler_1_set():
-    '''
+    """
     Returns an euler dictionary for 1 valid set
-    '''
-    assert spread_euler(
-        {
-            'a': [1, 2, 3]
-        }
-    ) == {'a': [1, 2, 3]}
+    """
+    assert spread_euler({"a": [1, 2, 3]}) == {"a": [1, 2, 3]}
 
 
 def test_spread_euler_2_sets_with_non_exclusivity():
-    '''
+    """
     Returns an euler dictionary for 2 valid sets
-    '''
+    """
 
-    assert spread_euler(
-        {
-            'a': [1],
-            'b': [1, 2]
-        }
-    ) == {'b': [2], 'a,b': [1]}
+    assert spread_euler({"a": [1], "b": [1, 2]}) == {"b": [2], "a,b": [1]}
 
 
 def test_spread_euler_2_sets():
-    '''
+    """
     Returns an euler dictionary for 2 valid sets
-    '''
-    assert spread_euler(
-        {
-            'a': [1, 2, 3],
-            'b': [2, 3, 4]
-        }
-    ) == {'b': [4], 'a,b': [2, 3], 'a': [1]}
+    """
+    assert spread_euler({"a": [1, 2, 3], "b": [2, 3, 4]}) == {
+        "b": [4],
+        "a,b": [2, 3],
+        "a": [1],
+    }
 
 
 def test_spread_euler_3_sets():
-    '''
+    """
     Returns an euler dictionary for 3 valid sets
-    '''
-    assert spread_euler(
-        {
-            'a': [1, 2, 3],
-            'b': [2, 3, 4],
-            'c': [3, 4, 5]
-        }) == {'a,b': [2], 'b,c': [4], 'a,b,c': [3], 'c': [5], 'a': [1]}
+    """
+
+    input_ = {"a": [1, 2, 3], "b": [2, 3, 4], "c": [3, 4, 5]}
+    expected_ = {
+        "a,b": [2],
+        "b,c": [4],
+        "a,b,c": [3],
+        "c": [5],
+        "a": [1],
+    }
+
+    assert spread_euler(input_) == expected_
 
 
 def test_spread_euler_4_sets():
-    '''
+    """
     Returns an euler dictionary for 4 valid sets
-    '''
-    assert spread_euler(
-        {
-            'a': [1, 2, 3],
-            'b': [2, 3, 4],
-            'c': [3, 4, 5],
-            'd': [3, 5, 6]
-        }) == {'a,b': [2], 'b,c': [4], 'a,b,c,d': [3], 'c,d': [5], 'd': [6], 'a': [1]}
+    """
+
+    input_ = {"a": [1, 2, 3], "b": [2, 3, 4], "c": [3, 4, 5], "d": [3, 5, 6]}
+
+    expected_ = {"a,b": [2], "b,c": [4], "a,b,c,d": [3], "c,d": [5], "d": [6], "a": [1]}
+
+    assert spread_euler(input_) == expected_
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,38 +1,39 @@
-from src.utils import (delimited_sort, keyfy, non_empty_sets_keys,
-                        reduce_, unique)
+from __future__ import annotations
+
+import src.utils as utils
 
 
 # define the tests
 def test_keyfy():
-    '''
+    """
     tests the key string generation
-    '''
-    assert keyfy([1, 2, 3]) == '1,2,3'
+    """
+    assert utils.keyfy([1, 2, 3]) == "1,2,3"
 
 
 def test_one_set_euler():
-    '''
+    """
     tests the reduce function
-    '''
-    assert reduce_(lambda a, b: a+b, [1, 2], 0) == 3
+    """
+    assert utils.reduce_(lambda a, b: a + b, [1, 2], 0) == 3
 
 
 def test_unique_elems():
-    '''
+    """
     tests the reduce function
-    '''
-    assert unique([1, 2, 3, 3]) == [1, 2, 3]
+    """
+    assert utils.unique([1, 2, 3, 3]) == [1, 2, 3]
 
 
 def test_delimited_sort():
-    '''
+    """
     tests sorting delimited by token
-    '''
-    assert delimited_sort('4,1,2,3', ',') == '1,2,3,4'
+    """
+    assert utils.delimited_sort("4,1,2,3", ",") == "1,2,3,4"
 
 
 def test_non_empty_sets_keys():
-    '''
+    """
     tests dict clean with empty values
-    '''
-    assert non_empty_sets_keys({'a': [1, 2, 3], 'b': []}) == ['a']
+    """
+    assert utils.non_empty_sets_keys({"a": [1, 2, 3], "b": []}) == ["a"]
