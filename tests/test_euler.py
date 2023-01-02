f7eb10cdfdaa9bdfae8a50afff16c9d56cb71013
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-02 11:16:45 -0300

    tests/ implement input-result-expectation test pattern explicitly

--- tests/test_euler.py
+++ tests/test_euler.py
@@ -9,35 +9,45 @@ def test_euler_iter_1_input():
     """
     Generates a tuple with key-value
     """
+    input_ = {"a": [1, 2]}
+    euler_gen = euler(input_)
+    expected_output = ("a", [1, 2])
 
-    assert next(euler({"a": [1, 2]})) == ("a", [1, 2])
+    assert next(euler_gen) == expected_output
 
 
 def test_euler_iter_2_input():
     """
     Generates all tuples with key-value
     """
-    eu_fun = euler({"a": [1, 2], "b": [2, 3]})
+    input_ = {"a": [1, 2], "b": [2, 3]}
+    euler_gen = euler(input_)
 
-    assert next(eu_fun) == ("b", [3])
-    assert next(eu_fun) == ("a,b", [2])
-    assert next(eu_fun) == ("a", [1])
+    assert next(euler_gen) == ("b", [3])
+    assert next(euler_gen) == ("a,b", [2])
+    assert next(euler_gen) == ("a", [1])
 
 
 def test_euler_iter_warning_1item():
     """
     Raises a warning for duplicated dict values
     """
+    input_ = {"a": [42, 42]}
+    euler_gen = euler(input_)
+
     with pytest.warns(UserWarning):
-        next(euler({"a": [42, 42]}))
+        next(euler_gen)
 
 
 def test_euler_iter_warning_2items():
     """
     Raises a warning for duplicated dict values
     """
+    input_ = {"a": [42, 42], "b": [42, 42]}
+    eule_gen = euler(input_)
+    
     with pytest.warns(UserWarning):
-        next(euler({"a": [42, 42], "b": [42, 42]}))
+        next(eule_gen)
 
 
 def test_spread_euler_ill_input_str():
@@ -60,26 +70,37 @@ def test_spread_euler_1_set():
     """
     Returns an euler dictionary for 1 valid set
     """
-    assert spread_euler({"a": [1, 2, 3]}) == {"a": [1, 2, 3]}
+    input_ = {"a": [1, 2, 3]}
+    result = spread_euler(input_)
+    expected_output = {"a": [1, 2, 3]}
+
+    assert result == expected_output
 
 
 def test_spread_euler_2_sets_with_non_exclusivity():
     """
     Returns an euler dictionary for 2 valid sets
     """
+    input_ = {"a": [1], "b": [1, 2]}
+    result = spread_euler(input_)
+    expected_output = {"b": [2], "a,b": [1]}
 
-    assert spread_euler({"a": [1], "b": [1, 2]}) == {"b": [2], "a,b": [1]}
+    assert result == expected_output
 
 
 def test_spread_euler_2_sets():
     """
     Returns an euler dictionary for 2 valid sets
     """
-    assert spread_euler({"a": [1, 2, 3], "b": [2, 3, 4]}) == {
+    input_ = {"a": [1, 2, 3], "b": [2, 3, 4]}
+    result = spread_euler(input_)
+    expected_output = {
         "b": [4],
         "a,b": [2, 3],
         "a": [1],
     }
+
+    assert result == expected_output
 
 
 def test_spread_euler_3_sets():
@@ -88,7 +109,8 @@ def test_spread_euler_3_sets():
     """
 
     input_ = {"a": [1, 2, 3], "b": [2, 3, 4], "c": [3, 4, 5]}
-    expected_ = {
+    result = spread_euler(input_)
+    expected_output = {
         "a,b": [2],
         "b,c": [4],
         "a,b,c": [3],
@@ -96,7 +118,7 @@ def test_spread_euler_3_sets():
         "a": [1],
     }
 
-    assert spread_euler(input_) == expected_
+    assert result == expected_output
 
 
 def test_spread_euler_4_sets():
@@ -105,7 +127,8 @@ def test_spread_euler_4_sets():
     """
 
     input_ = {"a": [1, 2, 3], "b": [2, 3, 4], "c": [3, 4, 5], "d": [3, 5, 6]}
+    
+    result = spread_euler(input_)
+    expected_output = {"a,b": [2], "b,c": [4], "a,b,c,d": [3], "c,d": [5], "d": [6], "a": [1]}
 
-    expected_ = {"a,b": [2], "b,c": [4], "a,b,c,d": [3], "c,d": [5], "d": [6], "a": [1]}
-
-    assert spread_euler(input_) == expected_
+    assert result == expected_output
