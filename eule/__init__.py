dc5156edf88044d5df53f220aecfa1adb4149fea
Bruno Peixoto <brunolnetto@gmail.com>
2023-11-21 16:39:44 -0300

    hotfix/ add feature references on __init__.py

--- eule/__init__.py
+++ eule/__init__.py
@@ -4,6 +4,8 @@
 
 from __future__ import annotations
 
+from eule.core import euler_generator, euler, \
+    euler_keys, euler_boundaries, Euler
 
 __author__ = """Bruno Peixoto"""
 __email__ = 'brunolnetto@gmail.com'
